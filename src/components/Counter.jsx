7652509e6848c0593d8123764c3a5326e8caa80a
Tyler <tydcarl@gmail.com>
2025-08-30 14:11:53 -0700

    works

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,32 +1,12 @@
 import React, { useState } from 'react';
 
 function Counter() {
-  const [cart, setCart] = useState({ 
-    item: 'apple', 
-    quantity: 0, 
-  });
-
-  function removeApple() {
-    setCart(prevCart => ({
-      ...prevCart, 
-      quantity: prevCart.quantity - 1, 
-    }))
-  }
-
-  function addApple() {
-    setCart(prevCart => ({
-      ...prevCart,
-      quantity: prevCart.quantity +1,
-
-    }))
-  }
   return (
     <div>
-      <button onClick={removeApple}>-</button>
-      {cart.quantity}
-      {cart.item}
-      <button onClick={addApple}>+</button>
+      <button>+</button>
+      <button>-</button>
     </div>
+
   );
 }
 
