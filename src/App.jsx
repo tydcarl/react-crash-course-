39d38d3b3471a06b1d1570676321b5e7f9d0a8c4
Tyler <tydcarl@gmail.com>
2025-09-01 16:09:27 -0700

    roues

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,23 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx";
-import Counter from "./components/Counter.jsx";
-import React, { useState, useEffect } from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    setShowModal(true);
-    console.log("onTodoDelete()");
-  }
-
-  function cancelModal() {
-    setShowModal(false);
-  }
-
-  function confirmModal() {
-    setShowModal(false);
-  }
-
-  useEffect(() => {
-    console.log("ONLY on mount");
-  }, []);
-
-
-  useEffect(() => {
-    console.log(`on mount AND on ${showModal} change`)
-  }, [showModal])
-
-  useEffect(() => {
-    console.log('EVERY render');
-  }, [showModal]);
-
   return (
     <div>
-      <Title />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}> Add Todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Finish FES"
-          paragraph="Code along with FrontendSimplified step by step"
-        />
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Finish Interview Section"
-          paragraph="Finish every interview question in the next six weeks"
-        />
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Land a $100k Job"
-          paragraph="Apply to 100 Jobs"
-        />
-      </div>
-      {showModal && (
-        <Modal
-          cancelModal={cancelModal}
-          confirmModal={confirmModal}
-          title="Confirm delete?"
-        />
-      )}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
+
 }
+
 
 export default App;
