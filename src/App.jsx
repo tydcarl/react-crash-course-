3bbc4c041115453208c0af3d1859df56bc5d70b2
Tyler <tydcarl@gmail.com>
2025-09-01 16:41:52 -0700

    1.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+import Nav from "./components/Nav.jsx";
+import Users from "./pages/Users.jsx";
 
 function App() {
   return (
-    <div>
       <Router>
-        <Routes>
+        <Nav />
+        <Routes> 
           <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/users/:username" element={<Users />} />
         </Routes>
       </Router>
-    </div>
+    
   );
 
 }
