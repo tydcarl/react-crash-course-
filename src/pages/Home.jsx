2321ace23342a2036b1d0ffcb604deb432468859
Tyler <tydcarl@gmail.com>
2025-09-06 13:53:08 -0700

    idk api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function Home() {
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data)
+    }
+   
     useEffect(() => {
-        ("https://jsonplaceholder.typicode.com/users")
+     fetchUsers();
     }, [])
-  return <h1>Home</h1>;
+  return <h1>{users[0]?.name}</h1>;
 }
 
 export default Home;
