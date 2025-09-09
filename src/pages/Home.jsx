2ae022a14ec366dadfa59456da487d68d1259a2d
Tyler <tydcarl@gmail.com>
2025-09-08 23:33:46 -0700

    shit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-    async function fetchUsers() {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-        setUsers(data)
-    }
-   
-    useEffect(() => {
-     fetchUsers();
-    }, [])
-  return <h1>{users[0]?.name}</h1>;
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {users.length > 0 ? <h1>{users[0]?.name}</h1> : <h1>Loading...</h1>}
+    </div>
+  );
 }
 
 export default Home;
